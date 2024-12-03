92b6e08fd3b8081ca8390ddb0fd102a5b0356e47
Tales <allen84324@gmail.com>
2024-12-04 01:09:53 +0800

    修改分頁路徑

--- src/routes/card-pdf.js
+++ src/routes/card-pdf.js
@@ -5,12 +5,11 @@ const router = express.Router();
 const prisma = new PrismaClient();
 
 // API：獲取 Deck 清單
-router.get("/deck-list", async (req, res) => {
+router.get("/cardPDF", async (req, res) => {
   try {
-    const decks = await prisma.deckList.findMany();
+    const decks = await prisma.deck_list.findMany();  // 查詢所有的 Deck 清單
     res.json(decks);
   } catch (error) {
-    console.error("錯誤：", error);
     res.status(500).send("伺服器錯誤");
   }
 });
