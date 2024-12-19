8db355d6378f5d263a9d79e0c8996f64adb53e56
YuCheng07 <aa139532058@gmail.com>
2024-12-19 14:37:58 +0800

    fix card-series api

--- src/routes/card-series.js
+++ src/routes/card-series.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
-router.get('/serise/:id', (req, res) => {
+router.get('/series/:id', (req, res) => {
     const seriesId = req.params.id;
     const seriesData = fs.readFileSync(`./src/API_ID/${ seriesId }.json`, 'utf8');
     res.status(200).send(seriesData);
 })
 
-router.get('/serise', (req, res) => {
+router.get('/series', (req, res) => {
     const seriesData = fs.readFileSync('./src/API_ID/series.json', 'utf8');
     res.status(200).send(seriesData);
 })
