ef329047c7a55711f497d3f62c8dee5fe69bbc3a
Enya-Wu <enyawu0119@gmail.com>
2024-12-29 21:24:47 +0800

    刪除牌組功能

--- src/routes/userDecks.js
+++ src/routes/userDecks.js
@@ -20,4 +20,24 @@ router.get('/decks', verifyToken, async (req, res) => {
   }
 });
 
+
+router.delete('/decks/:deck_id', verifyToken, async (req, res) => {
+  try {
+    const { deck_id } = req.params;
+
+    const deletedDeck = await prisma.deck_list.delete({
+        where: { deck_id },
+    });
+
+    res.json({ success: true, message: '刪除成功', deletedDeck });
+  } catch (error) {
+    if (error.code === 'P2003') {
+      res.status(400).json({ message: '已引用於文章,無法刪除' });
+    } else {
+      console.error('删除牌组失败:', error);
+      res.status(500).json({ message: '刪除失敗'});
+    }
+  }
+});
+
 module.exports = router;
